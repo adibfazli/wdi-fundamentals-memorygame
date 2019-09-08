1f65f637212938cc7a2addeb2fb388e493167176
Adib Fazli <adibfazli@Adibs-MacBook-Pro.local>
2019-09-08 16:24:15 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,5 @@
+var win = 0;
+var lost = 0;
 var cards = [
     {
         rank: "queen",
@@ -20,31 +22,69 @@ var cards = [
         cardImage: "images/king-of-diamonds.png"
     }
 ]
+
 var cardsInPlay = [];
+
+
+var cardElement= null;
+
+var game_board = document.getElementById('game_board');
+function createBoard(){
+    
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute("src","images/back.png");
+        cardElement.setAttribute('data-id',i);
+        cardElement.addEventListener("click",flipCard);
+        game_board.appendChild(cardElement);
+    }
+}
 
 function checkForMatch(){
     if (cardsInPlay[0] === cardsInPlay[1]){
         console.log("You found a match!")
         alert("You found a match!")
+        userWin();
     }
     else{
         console.log("Sorry, try again.")
         alert("Sorry, try again.")
+        userLost();
     }
 }
 
-function flipCard(cardId){
+function flipCard(){
+    var cardId = this.getAttribute('data-id')
     console.log("User flipped " + cards[cardId].rank);
     cardsInPlay.push(cards[cardId].rank)
     console.log(cards[cardId].suit)
     console.log(cards[cardId].cardImage)
-
+    
+    this .setAttribute('src',cards[cardId].cardImage)
+    
     if( cardsInPlay.length === 2){
         checkForMatch();
     }
 }
 
-flipCard(0);
-flipCard(2);
+let reset = document.querySelector(".reset").addEventListener("click", restart);
+createBoard();
 
+function restart(){
+    if (game_board.parentNode) {
+        // game_board.parentNode.removeChild(game_board);
+        win = 0;
+        lost = 0;
+        document.querySelector("#userWin").innerText= win;
+        document.querySelector("#userLost").innerText= lost ;
+    }
+}
+function userWin(){
+    win++;
+    document.querySelector("#userWin").innerText= win;
+}
+function userLost(){
+    lost++;
+    document.querySelector("#userLost").innerText= lost ;
+}
 
